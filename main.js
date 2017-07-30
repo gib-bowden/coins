62a079347c258c0ad5f5b7f2b77ef86431bc2f06
gib <gibbowden@gmail.com>
2017-07-29 20:35:07 -0500

    updated to account for bills

--- main.js
+++ main.js
@@ -15,31 +15,45 @@ function convertCentsToDollars(centAmount) {
 }
 
 /*
-gets the whole coin count for a given value and coin combination 
-	param coin = coin name found in the centValue and coinPurse objects
-	param inputAmount = starting amount in cents (e.g. 100 = $1.00) 
+gets the whole count for a given value and denomination combination 
+	param denomincation = denomincation found in the value and wallet objects
+	param inputAmountCents = starting amount in cents (e.g. 100 = $1.00) 
 */
-function getCoinCount(coin, inputAmountCents) {
-var countAsCents = inputAmountCents / centValue[coin];
-var coinCount = Math.floor(countAsCents);
-coinPurse[coin] = coinCount;
-centValue.running = inputAmountCents - (coinCount * centValue[coin]);
+function getDenominationCount(denomination, inputAmountCents) {
+var countAsCents = inputAmountCents / value[denomination];
+var count = Math.floor(countAsCents);
+wallet[denomination] = count;
+value.running = inputAmountCents - (count * value[denomination]);
 }
 
-//gets the count of all coin types for a given amount 
-function getTotalCoins(inputAmountCents) {
-	getCoinCount("quarter", inputAmountCents);
-	getCoinCount("dime", centValue.running);
-	getCoinCount("nickel", centValue.running);
-	getCoinCount("penny", centValue.running);
+//gets the count of all types for a given amount 
+function getTotalChange(inputAmountCents) {
+	getDenominationCount("hundred", inputAmountCents);
+	getDenominationCount("twenty", value.running);
+	getDenominationCount("ten", value.running);
+	getDenominationCount("five", value.running);
+	getDenominationCount("one", value.running);
+	getDenominationCount("quarter", value.running);
+	getDenominationCount("dime", value.running);
+	getDenominationCount("nickel", value.running);
+	getDenominationCount("penny", value.running);
 }
 
-//Calculates and outputs the count of all coin types for a user provied amount
-function coinCalculator(){
+//Calculates and outputs the count of all types for a user provied amount
+function changeCalculator(){
 var userAmountDollars = prompt("How much money you got?")
 var userAmountCents = convertDollarsToCents(userAmountDollars);
-getTotalCoins(userAmountCents);
-alert("You have " +coinPurse.quarter+ " quarters, " +coinPurse.dime+ " dimes, " +coinPurse.nickel+ " nickels, and " +coinPurse.penny+ " pennies!!")
+getTotalChange(userAmountCents);
+alert("You have " 
+	+wallet.hundred+ " hundreds, " 
+	+wallet.twenty+ " twenties, " 
+	+wallet.ten+ " tens, "
+	+wallet.five+ " fives, " 
+	+wallet.one+ " ones, "
+	+wallet.quarter+ " quarters, " 
+	+wallet.dime+ " dimes, " 
+	+wallet.nickel+ " nickels, and " 
+	+wallet.penny+ " pennies!!")
 }
 
 /*
@@ -47,24 +61,35 @@ alert("You have " +coinPurse.quarter+ " quarters, " +coinPurse.dime+ " dimes, " 
 */
 
 // object holds the value of each coin type and the running value used in the getTotalCoins function
-var centValue = {
-	quarter: 25,
-	dime: 10,
-	nickel: 5,
-	penny: 1,
-	running: 0
+var value = {
+		hundred: 10000,
+		fifty: 5000,
+		twenty: 2000,
+		ten: 10000,
+		five: 500,
+		one: 100,
+		quarter: 25,
+	    dime: 10,
+	    nickel: 5,
+	    penny: 1,
+	    running: 0
 }
 
-//object to hold the count for each coin type
-var coinPurse = {
-	quarter: 0,
-    dime: 0,
-    nickel: 0,
-    penny: 0	
+var wallet = {
+		hundred: 0,
+		fifty: 0,
+		twenty: 0,
+		ten: 0,
+		five: 0,
+		quarter: 0,
+	    dime: 0,
+	    nickel: 0,
+	    penny: 0
 }
 
 /*
 --------Workspace----------
 */
 
-coinCalculator();
+changeCalculator();
+
