ad121f10942c57644472913f98ebf20c3a1033a4
gib <gibbowden@gmail.com>
2017-08-01 20:32:27 -0500

    this is the last commit

--- main.js
+++ main.js
@@ -9,14 +9,15 @@ function convertDollarsToCents(dollarAmount) {
 }
 
 function getCoinCount(coin, inputAmountCents) {
-var countAsCents = inputAmountCents / centValue[coin];
-var coinCount = Math.floor(countAsCents);
-coinPurse[coin] = coinCount;
-return inputAmountCents -= (coinCount * centValue[coin]);
+	var countAsCents = inputAmountCents / centValue[coin];
+	var coinCount = Math.floor(countAsCents);
+	coinPurse[coin] = coinCount;
+	return inputAmountCents -= (coinCount * centValue[coin]);
 }
 
 //gets the count of all coin types for a given amount 
 function getTotalCoins(inputAmountCents) {
+	var coinPurseKeys = Object.keys(coinPurse);
 	var amount = inputAmountCents;	
 		for (var i = 0; i < coinPurseKeys.length; i++) {
 		amount = getCoinCount(coinPurseKeys[i], amount);
@@ -25,10 +26,10 @@ function getTotalCoins(inputAmountCents) {
 
 //Calculates and outputs the count of all coin types for a user provied amount
 function coinCalculator(){
-var userAmountDollars = prompt("How much money you got?")
-var userAmountCents = convertDollarsToCents(userAmountDollars);
-getTotalCoins(userAmountCents);
-alert("You have " +coinPurse.quarter+ " quarters, " +coinPurse.dime+ " dimes, " +coinPurse.nickel+ " nickels, and " +coinPurse.penny+ " pennies!!")
+	var userAmountDollars = prompt("How much money you got?")
+	var userAmountCents = convertDollarsToCents(userAmountDollars);
+	getTotalCoins(userAmountCents);
+	alert("You have " +coinPurse.quarter+ " quarters, " +coinPurse.dime+ " dimes, " +coinPurse.nickel+ " nickels, and " +coinPurse.penny+ " pennies!!")
 }
 
 /*
@@ -50,12 +51,6 @@ var coinPurse = {
     nickel: 0,
     penny: 0	
 }
-
-// turns the keys of the coinPurse object into an array
-var coinPurseKeys = Object.keys(coinPurse);
-
-// turns the keys of the coinValue object into an array
-var coinValueKeys = Object.keys(centValue);
 
 /*
 --------Workspace----------
